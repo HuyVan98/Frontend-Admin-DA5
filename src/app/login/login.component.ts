760aaa1e7118a5f251002655c426503dd0e3193e
HUY VAN <vanhuy98e1@gmail.com>
2021-12-29 00:09:47 +0700

    v1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { LoginService } from '../Services/login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  private API_URL = 'http://127.0.0.1:8000/api';
+
   constructor(
     private readonly loginService: LoginService,
   ) { }
@@ -20,14 +20,13 @@ export class LoginComponent implements OnInit {
     }
   }
   login() {
+
     var val = {
       email: this.email,
       password: this.password,
 
     };
     this.loginService.login(val).subscribe((data: any) => {
-      console.log(data.data);
-      
       if (data.status==1) {
         window.location.href = "";
         localStorage.setItem('user', JSON.stringify(data.data));
